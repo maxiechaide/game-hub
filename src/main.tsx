edf6829e0ce70c1cfb1f4017c581a9f90e38a055
Maxi-echaide <maxi.echaide@gmail.com>
2023-12-30 05:27:15 -0300

    build color mode switch

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import theme from './theme.ts'
-import { ChakraProvider, ColorModeContext, ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   
