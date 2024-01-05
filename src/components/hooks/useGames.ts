15ef2395a5e87094338e30cd60f02724a2767487
Maxi-echaide <maxi.echaide@gmail.com>
2024-01-04 21:34:02 -0300

    building game cards

--- src/components/hooks/useGames.ts
+++ src/components/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react';
 import apiClient from '../../services/api-client';
 import { CanceledError } from 'axios';
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string; //api doc
 }
 
 interface FetchGamesResponse {
@@ -14,24 +15,21 @@ interface FetchGamesResponse {
 
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    const controller = new AbortController()
+    const controller = new AbortController();
 
     try {
       apiClient
-        .get<FetchGamesResponse>('/games', {signal: controller.signal})
+        .get<FetchGamesResponse>('/games', { signal: controller.signal })
         .then((res) => setGames(res.data.results));
-      
     } catch (err) {
-      console.log(error)
+      console.log('fallo algo');
     }
-      
 
-      return () => controller.abort()
-      
-}, []);
+    return () => controller.abort();
+  }, []);
 
   return { games, error };
 };
